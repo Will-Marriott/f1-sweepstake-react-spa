6e3af5654ae5e55201a14f27a5f6982ad0aa9c61
William Marriott <wrjmarriott@gmail.com>
2025-06-07 09:32:35 +0100

    refactored header and table out to own components

---
+++ src/components/HeaderBar/HeaderBar.tsx
@@ -0,0 +1,11 @@
+const HeaderBar = () => {
+  return (
+    <div className="flex flex-col items-center w-full max-w-4xl p-2 bg-red-600 rounded-lg shadow-md">
+      <h1 className="text-2xl font-bold my-4 font-orbitron text-white">
+        F1 Sweepstake Drivers
+      </h1>
+    </div>
+  );
+};
+
+export default HeaderBar;
